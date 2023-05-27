0cce1bc421461ffe2274a5a4e31a2b00571a1b1c
mr-manson <nikola777jr@gmail.com>
2023-05-27 21:55:23 +0300

    try axios & fetch

--- src/components/Testingpage/Testingpage.jsx
+++ src/components/Testingpage/Testingpage.jsx
@@ -1,16 +1,23 @@
 import style from "./Testingpage.module.scss";
 import { GiTestTubes } from "react-icons/gi";
+/*import { useEffect, useState } from "react";*/
 /*import axios from "axios";*/
 
 const Testingpage = () => {
-
+/* ==================== with axios ==================== */
     /*axios.get("http://localhost:5000/api/todos", {
         withCredentials: true,
     })
-        .then(response => response.data)
+        .then(res => res.data)
         .then((data) => {console.log(data)})*/
-
-
+/* ==================== with fetch ==================== */
+    /*const [data, setData] = useState();
+    useEffect(() => {
+        fetch("http://localhost:5000/api/todos")
+            .then(res => res.json())
+            .then(result => setData(result));
+    })
+*/
 
     const link = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=";
     const trackLink = `${link}1FDCqZ0C5gig2lLRSEvnMRERBFdVCqieY`;
