951046a8992ce95ba730f806f0fd7bebc74b559a
mr-manson <nikola777jr@gmail.com>
2023-05-29 23:17:01 +0300

    fix: delete home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
-import Home from "./components/Home";
+import style from './App.module.scss';
+import Navigation from "./components/Navigation/Navigation";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/Main/Main";
+import Library from "./components/Library/Library";
+import Playlists from "./components/Playlists/Playlists";
+import TestingPage from "./components/TestingPage/TestingPage";
+import Player from "./components/Player/Player";
 
 function App() {
   return (
-    <Home />
+      <>
+        <div className={style.app_wrapper}>
+          <section className={style.main}>
+            <Navigation/>
+            <Routes>
+              <Route path="/" element={<Main/>} />
+              <Route path="/explore" element={<Library/>} />
+              <Route path="/playlists" element={<Playlists/>} />
+              <Route path="/test" element={<TestingPage/>} />
+            </Routes>
+          </section>
+          <Player/>
+        </div>
+      </>
   );
 }
 
--- src/components/TestingPage/TestingPage.jsx
+++ src/components/TestingPage/TestingPage.jsx
@@ -1,16 +1,16 @@
-import style from "./Testingpage.module.scss";
+import style from "./TestingPage.module.scss";
 import { GiTestTubes } from "react-icons/gi";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const TestingPage = () => {
+
     const [data, setData] = useState();
     useEffect(() => {
         (async function getData() {
             const res = await axios.get("http://localhost:5000/api/todo/1");
             setData(res.data.content);
         })()
-
     }, []);
 
 
@@ -27,7 +27,7 @@ const TestingPage = () => {
             <audio controls
                    src={trackLink}>
             </audio>
-            <div>Data from database - {data}</div>
+            <div>Data from database - {data ? data : "nothing"}</div>
         </div>
     )
 }
